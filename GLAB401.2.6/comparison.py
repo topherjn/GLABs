f8d2cb7be745b145ee9fd5412eac7b03a5dbfebe
topherjn <nicholsonc@gmail.com>
2024-01-24 17:16:34 -0500

    comparison and logical done

---
+++ GLAB401.2.6/comparison.py
@@ -0,0 +1,9 @@
+value1 = 5
+value2 = 2
+
+print(value1 == value2)
+print(value1 != value2)
+print(value1 < value2)
+print(value1 > value2)
+print(value1 <= value2)
+print(value1 >= value2)
---
+++ GLAB401.2.6/logical.py
@@ -0,0 +1,7 @@
+is_sunny = False
+is_warm = False
+
+print(is_sunny and is_warm)
+print(is_sunny or is_warm)
+print(not is_sunny and is_warm)
+print(not is_sunny or is_warm)
