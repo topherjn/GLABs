f6700543afcde0d6943496b5ad1ceb1ad591e973
topherjn <nicholsonc@gmail.com>
2024-01-24 11:05:09 -0500

    sorts complete

--- GLAB401.2.4/sortingquest.py
+++ GLAB401.2.4/sortingquest.py
@@ -22,8 +22,6 @@ number_list.sort(reverse=True)
 print(f"Built-in sorted descending: {number_list}")
 
 # bubble sort
-
-# create a swap list items function for both kinds of sorts
 # note - no tweaks for efficiency
 def bubble_sort_list(list_to_sort):
 
@@ -37,6 +35,26 @@ def bubble_sort_list(list_to_sort):
                 list_to_sort[i] = list_to_sort[j]
                 list_to_sort[j] = temp
 
+def insertion_sort_list(list_to_sort):
+
+    num_items = len(list_to_sort)
+
+    for i in range(1,num_items):
+        # place each item one at a time
+        item_to_place = list_to_sort[i]
+
+        # get index of item in front of i
+        j = i - 1
+
+        # while the item to place is in the wrong
+        # order, push everything back
+        while j >= 0 and item_to_place < list_to_sort[j]:
+            list_to_sort[j+1] = list_to_sort[j]
+            j -= 1
+        # once everything is smaller than item
+        # drop it in
+        list_to_sort[j+1] = item_to_place
+
 # test bubble
 number_list = []
 for i in range(0, 20):
@@ -46,11 +64,16 @@ print(f"Original pseudorandom list of integers: {number_list}")
 
 bubble_sort_list(number_list)
 
-print(f"After bubble_sort: {number_list}")
+print(f"After bubble sort: {number_list}")
 
-# insertion sort
+# test insertion sort
 number_list = []
 for i in range(0, 20):
     number_list.append(rand.randint(1,100))
-    
+
 print(f"Original pseudorandom list of integers: {number_list}")
+
+insertion_sort_list(number_list)
+
+print(f"After insertion sort: {number_list}")
+
