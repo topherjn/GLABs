ef9416cee4ae4521c1e114ab87bde749bca99892
topherjn <nicholsonc@gmail.com>
2024-01-24 10:21:10 -0500

    added instructional comments

--- GLAB401.2.4/sortingquest.py
+++ GLAB401.2.4/sortingquest.py
@@ -1,2 +1,14 @@
-import random
+"""
+The Quest for Sorting: Create a program that takes a list of numbers 
+and sorts them in ascending or descending order. Challenge yourself 
+to implement sorting algorithms like bubble sort or insertion sort.
+"""
+
+# create list of numbers.  random?
+
+# use built-in sorting
+
+# bubble sort
+
+# insertion sort
 
