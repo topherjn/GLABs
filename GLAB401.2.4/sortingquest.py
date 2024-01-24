290636ae954e95f9dad03af1ef2ce5712e41221f
topherjn <nicholsonc@gmail.com>
2024-01-24 10:26:42 -0500

    random number list of ints created

--- GLAB401.2.4/sortingquest.py
+++ GLAB401.2.4/sortingquest.py
@@ -5,7 +5,14 @@ to implement sorting algorithms like bubble sort or insertion sort.
 """
 
 # create list of numbers.  random?
+import random as rand
 
+number_list = []
+
+for i in range(0, 20):
+    number_list.append(rand.randint(1,100))
+
+print(number_list)
 # use built-in sorting
 
 # bubble sort
