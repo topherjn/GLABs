f2c83ca043e12ceca45c15702a79b1daa68b372d
topherjn <nicholsonc@gmail.com>
2024-01-24 12:09:34 -0500

    translation done

---
+++ GLAB401.2.5/translation_quest.py
@@ -0,0 +1,19 @@
+"""
+The Quest for Translation: Create a program that acts as a translator. Use a dictionary to store words and their translations. Prompt the user to enter a word and display its translation if it exists in the dictionary.
+"""
+translation_dictionary = {}
+translation_dictionary['apple'] = 'pomme'
+translation_dictionary['man'] = 'homme'
+translation_dictionary['woman'] = 'femme'
+translation_dictionary['red'] = 'rouge'
+translation_dictionary['sky'] = 'ciel'
+translation_dictionary['earth'] = 'terre'
+translation_dictionary['keyboard'] = 'clavier'
+translation_dictionary['tooth'] = 'dent'
+translation_dictionary['water'] = 'eau'
+translation_dictionary['eye'] = 'oeil'
+
+
+word = input("Enter an English word to translate into French: ")
+
+print(f"{word} is {translation_dictionary[word]} in French")
