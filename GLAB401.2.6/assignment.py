c81f9e73c830ed845d0d6a866e17fde0946b0899
topherjn <nicholsonc@gmail.com>
2024-01-25 16:33:54 -0500

    made assignment.py prettier

--- GLAB401.2.6/assignment.py
+++ GLAB401.2.6/assignment.py
@@ -1,21 +1,25 @@
+
+
 x = 1
 y = 2
-print(x,y)
+
+print("x was assigned the value",x)
+print("y was assigned the value", y)
 
 x = y + 1
-print(x,y)
+print('y + 1 ==', x)
 
 x += y
-print(x,y)
+print("x += y ==",x)
 
 y -= x
-print(x,y)
+print("y -= x ==", y)
 
 x *= y
-print(x,y)
+print("x *= y ==", x)
 
 x /= y
-print(x,y)
+print("x /= y ==", x)
 
 x**=y
-print(x,y)
+print("x ^ y ==",x)
