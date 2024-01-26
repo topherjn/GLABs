875634f84aeec888c9fd660805a94193eec347c3
topherjn <nicholsonc@gmail.com>
2024-01-26 11:18:28 -0500

    troll riddle done

---
+++ GLAB401.2.1/troll.py
@@ -0,0 +1,8 @@
+answer = 'what do you mean? an african or european swallow?'
+
+guess = input('What is the air-speed velocity of an unladen swallow? ')
+
+if guess.lower() == answer:
+    print("The troll is sent flying into the chasm.")
+else:
+    print("You are sent flying into the chasm.")
