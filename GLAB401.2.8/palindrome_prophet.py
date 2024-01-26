0fc4170436d82cd15f6240cb22d87586a5470de9
topherjn <nicholsonc@gmail.com>
2024-01-26 15:50:06 -0500

    since it never changes I made the filter list into a tuple

--- GLAB401.2.8/palindrome_prophet.py
+++ GLAB401.2.8/palindrome_prophet.py
@@ -1,7 +1,7 @@
 # Create a new Python file named "palindrome_prophet.py."
     # done
 # Define a function that checks if a given string is a palindrome.
-alphanumeric = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"]
+alphanumeric = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0")
 def is_palidrome(phrase):
     phrase = phrase.lower()
     filtered_phrase = ''
