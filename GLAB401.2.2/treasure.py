736a22d87185f9d88ac2b752b783d9693d42d0fe
topherjn <nicholsonc@gmail.com>
2024-01-26 11:42:29 -0500

    added comments but this caused fake errors in VS Code

--- GLAB401.2.2/treasure.py
+++ GLAB401.2.2/treasure.py
@@ -5,11 +5,15 @@ treasure = 65
 
 guess = 0
 
+# ask use for whole number in regular numerals
+# if they type something silly yell at user
+# this is a primer for the loop
 try:
     guess = int(input('What number do you think hides the treasure (1 - 100)? '))
 except ValueError:
     print("Please enter a whole number in Hindu numerals. ")
 
+# as long as the user is wrong let them know how close they are
 while guess != treasure:
     if abs(treasure - guess) < 5:
         print("You're red hot!")
@@ -17,7 +21,8 @@ while guess != treasure:
         print("You're getting warmer!")
     else:
         print("Ice cold!")
-
+\
+    # set up the next guess
     try:
         guess = int(input('What number do you think hides the treasure (1 - 100)? '))
     except ValueError:
@@ -25,5 +30,5 @@ while guess != treasure:
 
 
 
-
+# loop ends when they guess the answer
 print("You found the treasure! ")
