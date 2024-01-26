b9d254b58aa11928ca6efad2759d730884e7895f
topherjn <nicholsonc@gmail.com>
2024-01-26 10:32:07 -0500

    open sesame done

---
+++ GLAB401.2.1/if_else_adventure.py
@@ -0,0 +1,7 @@
+# The Enchanted Door
+password = input("Enter the secret password: ")
+
+if password == "Open Sesame":
+    print("The door opens wide. Welcome!")
+else:
+    print("The door remains closed. Access denied!")
