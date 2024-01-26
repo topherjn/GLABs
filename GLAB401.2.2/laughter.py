ad2ea95dd88dfa29a6d8c0a3e1cbd6c2f9e566d2
topherjn <nicholsonc@gmail.com>
2024-01-26 12:31:24 -0500

    jokes added to laughter

---
+++ GLAB401.2.2/laughter.py
@@ -0,0 +1,12 @@
+
+# create a python list of jokes from the jokes here:
+# https://datasciencedojo.com/blog/data-science-jokes/#
+jokes = ["There are two kinds of data scientists. 1.) Those who can extrapolate from incomplete data.",
+"Data science is 80% preparing data, and 20% complaining about preparing data.",
+"There are 10 kinds of people in this world. Those who understand binary and those who don't.", "What's the difference between an introverted data analyst & an extroverted one? Answer: the extrovert stares at YOUR shoes.",
+"Why did the chicken cross the road? The answer is trivial and is left as an exercise for the reader.",
+"The data science motto: If at first, you don't succeed; call it version 1.0",
+"What do you get when you cross a pirate with a data scientist? Answer: Someone who specializes in Rrrr",
+'A SQL query walks into a bar, walks up to two tables, and asks, "Can I join you?"',
+'Why should you take a data scientist with you into the jungle? Answer: They can take care of Python problems','Old data analysts never die - they just get broken down by age']
+
