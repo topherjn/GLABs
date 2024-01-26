a415278ae6f4d8848c594adc8c3ae3a61f372b90
topherjn <nicholsonc@gmail.com>
2024-01-26 15:21:13 -0500

    tweaked biggest word

--- GLAB401.2.8/word_wrangler.py
+++ GLAB401.2.8/word_wrangler.py
@@ -7,11 +7,12 @@ words = sentence.split()
 print(len(words))
 # Identify and print the longest word in the sentence.
 biggest_word = words[0]
-max = len(words[0])
+max = len(biggest_word)
 
 for word in words[1:]:
     if len(word) > max:
         biggest_word = word
+        max = len(word)
 print("Longest word: " + biggest_word)
 
 # Capitalize the first letter of each word in the sentence.
