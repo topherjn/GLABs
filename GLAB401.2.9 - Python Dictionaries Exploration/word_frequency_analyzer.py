8fb4a52d7f1104edd9c83646a63d59245d95d91c
topherjn <nicholsonc@gmail.com>
2024-01-27 14:09:43 -0500

    word frequency done

--- GLAB401.2.9 - Python Dictionaries Exploration/word_frequency_analyzer.py
+++ GLAB401.2.9 - Python Dictionaries Exploration/word_frequency_analyzer.py
@@ -1,13 +1,34 @@
 # determines word frequencies in user input.
 # type 'quit' to exit program
 freq = {}
-line = input()
-while line != 'quit':
-  words = line.split()
-  for word in words:
-    freq[word] = freq.get(word, 0) + 1
-  line = input()
+text = 'Four score and seven years ago our fathers \
+brought forth, upon this continent, a new nation, \
+conceived in liberty, and dedicated to theproposition \
+that all men are created equal. Now we are engaged in \
+a great civil war, testing whether that nation, or \
+any nation so conceived, and so dedicated, can long \
+endure. We are met on a great battle field of that \
+war. We come to dedicate a portion of it, as a final \
+resting place for those who died here, that the nation \
+might live. This we may, in all propriety do.'
 
-# when 'quit' detected:
+text = text.lower()
+
+letters = ('a','b','c','d','e','f','g','h','i','j','k'
+           ,'l','m','n','o','p','q','r','s','t','u',
+           'v','w','x','y','z',' ')
+
+filtered_words = ''
+for character in text:
+   if character in letters:
+      filtered_words += character
+
+print(filtered_words)
+
+words = filtered_words.split()
+
+for word in words:
+    freq[word] = freq.get(word, 0) + 1
+
 for word in freq:
-  print(word), ': ', freq[word]
+  print(word, ': ', freq[word])
