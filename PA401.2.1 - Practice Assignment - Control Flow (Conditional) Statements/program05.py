c64e1601caf9c09be10dfb1c50d94af7e7420ce7
topherjn <nicholsonc@gmail.com>
2024-01-27 16:46:31 -0500

    score gradey thing done

---
+++ PA401.2.1 - Practice Assignment - Control Flow (Conditional) Statements/program05.py
@@ -0,0 +1,15 @@
+# get grade from user and convert it to an integer
+score = int(input('Enter the numeric grade: '))
+
+if score >= 90 and score <= 100:
+    print('A')
+elif score >= 80 and score <= 89:
+    print('B')
+elif score >= 70 and score <= 79:
+    print('C')
+elif score >= 60 and score <= 69:
+    print('D')
+elif score >= 0:
+    print('F')
+else:
+    print("Invalid score")
