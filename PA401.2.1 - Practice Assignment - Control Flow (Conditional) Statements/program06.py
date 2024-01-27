af7daa92d8ab66c1b2ec12dc9e391729b2808652
topherjn <nicholsonc@gmail.com>
2024-01-27 17:28:31 -0500

    Whole lab done

--- PA401.2.1 - Practice Assignment - Control Flow (Conditional) Statements/program06.py
+++ PA401.2.1 - Practice Assignment - Control Flow (Conditional) Statements/program06.py
@@ -4,13 +4,15 @@ statuses = ('Single',
             'Married Filing Separately',
             'Head of Household')
 
+# rates for each status didn't strain to hard to make sure ranges right
+# painful data entry job
+# this allows for simplying if statement
 single_rates = (0,8350,8351,33950,33951,82250,82251,171550,171551,372950,372951)
 joint_rates = (0,16700,16701,67900,67901,137050,137051,208850,208851,372950,372951)
 separate_rates = (0,8350,8351,33950,33951,68525,68526,104425,104256,186475,186476)
 head_rates = (0,11950,11951,45500,45501,117450,117451,190200,190201,372950,372951)
 
 # get status from user
-
 print("Filing statuses: ")
 for status in statuses:
     print(status)
@@ -27,7 +29,7 @@ while income < 0:
     print("Your expenses don't count")
     income = float(input("Enter your income: "))
 
-tax_bracket = ()
+# set the ranges of income based on the status
 if status == statuses[0]:
     tax_bracket = single_rates
 elif status == statuses[1]:
@@ -37,6 +39,7 @@ elif status == statuses[2]:\
 elif status == statuses[3]:
     tax_bracket = head_rates
 
+# find out the rate
 marginal_rate = 0
 if income >= tax_bracket[0] and income <= tax_bracket[1]:
     marginal_rate = 10
@@ -51,5 +54,5 @@ if income >= tax_bracket[8] and income <= tax_bracket[9]:
 if income >= tax_bracket[10]:
     marginal_rate = 35
 
-
+# test
 print(f'For an income of {income} filing {status}, your marginal tax rate is {marginal_rate}%')
