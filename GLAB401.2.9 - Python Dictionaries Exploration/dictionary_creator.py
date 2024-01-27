86670df0e1803b92d659be2e9e2e016f889ac910
topherjn <nicholsonc@gmail.com>
2024-01-27 12:18:56 -0500

    pet dictionary created with some errors

---
+++ GLAB401.2.9 - Python Dictionaries Exploration/dictionary_creator.py
@@ -0,0 +1,11 @@
+# create dictionary of pet nicknames and "real names"
+pet_names = { 'Crispie' : 'Charles Crispen Peabody', 
+              'Brownie' : 'Elisabeth Barrett Brownie',
+              'Granny-Baby' : 'Vegas',
+              'Mommadawg' : 'Sugary',
+              'Shelly' : 'Shelton Allen'}
+
+
+
+
+
