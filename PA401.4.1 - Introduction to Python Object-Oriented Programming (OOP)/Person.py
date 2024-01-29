3ce05428e757beb48fd9e3630df73ffc5fb1807c
topherjn <nicholsonc@gmail.com>
2024-01-29 18:23:27 -0500

    changed meaning of constructor messages

--- PA401.4.1 - Introduction to Python Object-Oriented Programming (OOP)/Person.py
+++ PA401.4.1 - Introduction to Python Object-Oriented Programming (OOP)/Person.py
@@ -3,15 +3,15 @@ class Person:
     def __init__(self,name,age) -> None:
         self.name = name
         self.age = age
-
-        print(f"\nName: {self.name}, Age: {self.age}", end="")
-
+        
+        print("Person object created")
 # Task 2
 class Student(Person):
     def __init__(self, name, age, student_id):
         super().__init__(name, age)
         self.student_id = student_id
-        print(f", Student ID: {self.student_id}", end="")
+
+        print("Student object created")
         
 
 # testing
