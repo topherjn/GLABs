d25446a36d3151e61ab8a249dfd2d47434abcc70
topherjn <nicholsonc@gmail.com>
2024-01-29 18:00:39 -0500

    Task 1 done

--- PA401.4.1 - Introduction to Python Object-Oriented Programming (OOP)/Person.py
+++ PA401.4.1 - Introduction to Python Object-Oriented Programming (OOP)/Person.py
@@ -1,6 +1,15 @@
 class Person:
-    pass
+    def __init__(self,name,age) -> None:
+        self.name = name
+        self.age = age
 
+    def print_person(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+        
+
+# testing
 if __name__ == "__main__":
-    person = Person()
-    print(person)
+    person = Person('Tav',2)
+    person.print_person()
+    
