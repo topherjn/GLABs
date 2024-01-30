0347bd00b85435c6e7172ab9ae5dbdd10f174b91
topherjn <nicholsonc@gmail.com>
2024-01-29 20:53:09 -0500

    task 1 done

--- GLAB401.4.2 - Exploring Inheritance, Encapsulation, and Polymorphism in Python/Vehicles.py
+++ GLAB401.4.2 - Exploring Inheritance, Encapsulation, and Polymorphism in Python/Vehicles.py
@@ -4,22 +4,36 @@ class Vehicle:
         self.model = model
         self.year = year
 
+# add a type (like sedan or whatever)
 class Car(Vehicle):
     def __init__(self, make, model, year, type):
         super().__init__(make, model, year)
         self.type = type
 
+# add number of wheels
 class Truck(Vehicle):
     def __init__(self, make, model, year, num_wheels):
         super().__init__(make, model, year)
         self.num_wheels = num_wheels
 
+# add displacement in cubic centimeters
 class MotorCycle(Vehicle):
     def __init__(self, make, model, year, cc):
         super().__init__(make, model, year)
         self.cc = cc
 
-vehicles = [Car('Mazda', 'Miata', 2006, 'Sport'), Truck('Cheverolt', 'S-10', 1984, 4), MotorCycle('Indian', 'Scout', 1920, 606)]
+# test
+vehicles = [Car('Mazda', 'Miata', 2006, 'Sport'), Truck('Cheverolet', 'S-10', 1984, 4), MotorCycle('Indian', 'Scout', 1920, 606)]
+
+for vehicle in vehicles:
+    print(f"Make: {vehicle.make}, Model: {vehicle.model}, Year: {vehicle.year}")
+    # downcast for specifics for each derived class
+    if isinstance(vehicle,Car):
+        print(f"Type: {vehicle.type}")
+    if isinstance(vehicle,Truck):
+        print(f"Number of Wheels: {vehicle.num_wheels}")
+    if isinstance(vehicle,MotorCycle):
+        print(f"Displacement in Cubic Centimeters: {vehicle.cc}")
 
 
 
