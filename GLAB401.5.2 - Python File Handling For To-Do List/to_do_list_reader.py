33bd0025375694d3a8bc7a129a4adc0fc2dd0e52
topherjn <nicholsonc@gmail.com>
2024-01-31 16:34:33 -0500

    Added file does not exist

--- GLAB401.5.2 - Python File Handling For To-Do List/to_do_list_reader.py
+++ GLAB401.5.2 - Python File Handling For To-Do List/to_do_list_reader.py
@@ -4,14 +4,15 @@ Use the "with" statement for file handling to ensure proper resource management.
 Allow the user to input the filename they want to read for the to-do list.
 Display a user-friendly message if the file doesn't exist.
 """
-# note: I am saving error checking for future tasks
-
-# make sure it's the current directory for this project on my machine
-path = "D:\\Per Scholas\\GLABs\\GLAB401.5.2 - Python File Handling For To-Do List\\"
-
+import os
 # prompt user 
 # save user response in variable
 filename = input("What is the name of the file you want to open? ")
+
+# if the user messes up yell at user
+while not os.path.isfile(filename):
+    print("Sorry, that file does not exist.  Try again.")
+    filename = input("What is the name of the file you want to open? ")
 
 # open a file in the cwd with user's input
 # will crask on File Not Found
