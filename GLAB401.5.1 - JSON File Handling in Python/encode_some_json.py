ea400b02dd8148bb1174126f98429d883a7d26ca
topherjn <nicholsonc@gmail.com>
2024-02-03 18:56:33 -0500

    obtained set of coords for one feature

--- GLAB401.5.1 - JSON File Handling in Python/encode_some_json.py
+++ GLAB401.5.1 - JSON File Handling in Python/encode_some_json.py
@@ -1,2 +1,16 @@
 import json
 
+# task 2
+
+with (open("arrondissements.json", "r")) as file:
+    paris_arrondissements = json.load(file)
+
+for feature in paris_arrondissements['features']:
+    geometry = feature['geometry']
+
+    for coords in geometry['coordinates']:
+        list_coords = coords
+
+        for coord in list_coords:
+            print(coord)
+        
