fc07f8c0e477a2f40ed62caab458753dab0d61a9
topherjn <nicholsonc@gmail.com>
2024-02-03 19:10:35 -0500

    dump geometry coords to json file

--- GLAB401.5.1 - JSON File Handling in Python/encode_some_json.py
+++ GLAB401.5.1 - JSON File Handling in Python/encode_some_json.py
@@ -5,12 +5,24 @@ import json
 with (open("arrondissements.json", "r")) as file:
     paris_arrondissements = json.load(file)
 
+# this shows the border coordinates for each
+# feature
 for feature in paris_arrondissements['features']:
     geometry = feature['geometry']
+    # name of location
+    print(feature['properties']['l_aroff'])
 
+    # coords for location
     for coords in geometry['coordinates']:
+        
+        # coords in a list
         list_coords = coords
-
-        for coord in list_coords:
+        # individual long/lat pairs
+        for coord in list_coords:    
             print(coord)
+
+# task 1 - encode geometry dictionary to json
+with (open("geometry.json", "w")) as file:
+    json.dump(geometry, file)
+           
         
