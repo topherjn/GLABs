7a0d5edeb5abf0712cf8784da96e1ceca330cd69
topherjn <nicholsonc@gmail.com>
2024-02-07 18:16:58 -0500

    GLAB401.8.1-students table created

---
+++ GLAB401.8.1 - Python Database Integration with ORM and Advanced Queries/databaseops.py
@@ -0,0 +1,64 @@
+""" Task 1: Database Connection"""
+
+# connector already installed, will use it here to create
+# operations_db database
+
+# create connector object
+import mysql.connector
+try:
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+       
+    )
+
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
+
+# build create db command
+query = 'CREATE DATABASE IF NOT EXISTS operations_db'
+
+# create the cursor object for the connectoin
+cursor = conn.cursor()
+
+# execute the command
+cursor.execute(query)
+
+# being using this db
+cursor.execute("USE operations_db")
+
+"""Task 2: CRUD Operations"""
+
+# Create
+try:
+    cursor = conn.cursor()
+    create_table_query = """
+    CREATE TABLE students (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(50),
+        age INT,
+        PRIMARY KEY (id)
+    )    
+    """
+    cursor.execute(create_table_query)
+    print("Table 'students' created successfully")
+
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
+
+finally:
+    cursor.close()
+try:
+    cursor = conn.cursor()
+    create_table_query = """ """
+    cursor.execute(create_table_query)
+    print("Table 'students' created successfully")
+
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
+
+finally:
+    cursor.close()
+
+
